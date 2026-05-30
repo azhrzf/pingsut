be77607fb448c8c4abf30732c1847ffc45e8c8ed
Azhar Zaidan Fauzi <a.zaidan.f.azf616@gmail.com>
2026-05-30 15:53:20 +0700

    chore: remove leftover db integration

--- src/Pingsut.Domain/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-using Pingsut.Domain.Common;
-using Pingsut.Domain.Entities.Common;
-
-namespace Pingsut.Domain.Entities;
-
-public class User : AuditableEntity
-{
-    public required string FirebaseId { get; set; }
-}
--- src/Pingsut.Persistence/Repositories/UserRepository.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using Pingsut.App.Contracts.Persistence;
-using Pingsut.Domain.Entities;
-using Pingsut.Persistence.DbContexts;
-
-namespace Pingsut.Persistence.Repositories;
-
-public class UserRepository(PostgreDbContext postgreDbContext)
-    : BaseRepository<User>(postgreDbContext), IUserRepository;
